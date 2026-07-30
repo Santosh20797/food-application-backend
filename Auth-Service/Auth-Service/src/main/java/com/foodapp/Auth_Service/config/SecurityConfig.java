7b3436516d722f23600fcd27f5b810ce54a84662
Santosh Kumar Pal <santoshsiet91@gmail.com>
2026-07-30 17:56:14 +0530

    this is the auth service

--- Auth-Service/Auth-Service/src/main/java/com/foodapp/Auth_Service/config/SecurityConfig.java
+++ Auth-Service/Auth-Service/src/main/java/com/foodapp/Auth_Service/config/SecurityConfig.java
@@ -50,13 +50,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/api/auth/**",
-                                "/swagger-ui/**",
-                                "/swagger-ui.html",
-                                "/v3/api-docs/**"
-                        ).permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 );
 
         return http.build();
