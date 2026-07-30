13394a2599276db71b9624b9f9d0425e51cf40d9
Santosh Kumar Pal <santoshsiet91@gmail.com>
2026-07-30 17:32:02 +0530

    this is the auth service

--- Auth-Service/Auth-Service/src/main/java/com/foodapp/Auth_Service/config/SecurityConfig.java
+++ Auth-Service/Auth-Service/src/main/java/com/foodapp/Auth_Service/config/SecurityConfig.java
@@ -5,6 +5,35 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                // Disable CSRF protection since we are building a stateless REST API
+//                .csrf(csrf -> csrf.disable())
+//
+//                // Configure route authorization rules
+//                .authorizeHttpRequests(auth -> auth
+//                        // Allow anyone to access the register and login routes without a password
+//                        .requestMatchers("/api/auth/**").permitAll()
+//                        // Any other request across this service must be authenticated
+//                        .anyRequest().authenticated()
+//                );
+//
+//        return http.build();
+//    }
+//}
+
+
 
 @Configuration
 @EnableWebSecurity
@@ -17,15 +46,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                // Disable CSRF protection since we are building a stateless REST API
-                .csrf(csrf -> csrf.disable())
 
-                // Configure route authorization rules
+        http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        // Allow anyone to access the register and login routes without a password
-                        .requestMatchers("/api/auth/**").permitAll()
-                        // Any other request across this service must be authenticated
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 );
 
